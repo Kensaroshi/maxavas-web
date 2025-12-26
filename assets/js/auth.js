4d4204e3f2e4e539365f93aa3d08b056924b039a
MaxAvas <116870378+Kensaroshi@users.noreply.github.com>
2025-12-26 19:00:10 +0700

    Delete assets/js/auth.js

--- assets/js/auth.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  localStorage.setItem("loggedIn", "true");
-  window.location.href = "index.html";
-}
-
-function logout() {
-  localStorage.removeItem("loggedIn");
-  window.location.href = "login.html";
-}
